144a20b8836acc2d4bf1840bdc3528f4751c837b
zoe <zoestudio18@gmail.com>
2021-04-29 15:15:55 +1000

    create a new xcode project

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Hua Zuo on 29/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
